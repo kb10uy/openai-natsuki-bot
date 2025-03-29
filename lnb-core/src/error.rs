f973b79fd38083454db607140a9084c9ec73d983
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-03-30 03:20:32 +0900

    by_xxxx を生やす

--- lnb-core/src/error.rs
+++ lnb-core/src/error.rs
@@ -2,6 +2,8 @@ use std::error::Error as StdError;
 
 use thiserror::Error as ThisError;
 
+pub type ErasedError = Box<dyn StdError + Send + Sync + 'static>;
+
 /// LnbClient のエラー。
 #[derive(Debug, ThisError)]
 pub enum ClientError {
@@ -11,11 +13,21 @@ pub enum ClientError {
 
     /// 通信関連のエラー。
     #[error("communication failed: {0}")]
-    Communication(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    Communication(#[source] ErasedError),
 
     /// 外部 API などのエラー。
     #[error("external error: {0}")]
-    External(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    External(#[source] ErasedError),
+}
+
+impl ClientError {
+    pub fn by_communication(source: impl Into<ErasedError>) -> ClientError {
+        ClientError::Communication(source.into())
+    }
+
+    pub fn by_external(source: impl Into<ErasedError>) -> ClientError {
+        ClientError::External(source.into())
+    }
 }
 
 /// LnbServer のエラー。
@@ -39,10 +51,10 @@ pub enum ServerError {
 #[derive(Debug, ThisError)]
 pub enum LlmError {
     #[error("communication failed: {0}")]
-    Communication(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    Communication(#[source] ErasedError),
 
     #[error("backend error: {0}")]
-    Backend(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    Backend(#[source] ErasedError),
 
     /// LLM が有効なレスポンスを生成しなかった。
     #[error("no choice returned")]
@@ -50,25 +62,59 @@ pub enum LlmError {
 
     /// JSON の復元ができない。
     #[error("invalid response format: {0}")]
-    ResponseFormat(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    ResponseFormat(#[source] ErasedError),
+}
+
+impl LlmError {
+    pub fn by_communication(source: impl Into<ErasedError>) -> LlmError {
+        LlmError::Communication(source.into())
+    }
+
+    pub fn by_backend(source: impl Into<ErasedError>) -> LlmError {
+        LlmError::Backend(source.into())
+    }
+
+    pub fn by_format(source: impl Into<ErasedError>) -> LlmError {
+        LlmError::ResponseFormat(source.into())
+    }
 }
 
 /// Storage 層のエラー。
 #[derive(Debug, ThisError)]
 pub enum StorageError {
     #[error("backend error: {0}")]
-    Backend(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    Backend(#[source] ErasedError),
 
     #[error("serialization error: {0}")]
-    Serialization(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    Serialization(#[source] ErasedError),
+}
+
+impl StorageError {
+    pub fn by_serialization(source: impl Into<ErasedError>) -> StorageError {
+        StorageError::Backend(source.into())
+    }
+
+    pub fn by_backend(source: impl Into<ErasedError>) -> StorageError {
+        StorageError::Backend(source.into())
+    }
 }
 
 /// Function 層のエラー。
 #[derive(Debug, ThisError)]
 pub enum FunctionError {
     #[error("serialization error: {0}")]
-    Serialization(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    Serialization(#[source] ErasedError),
 
     #[error("external error: {0}")]
-    External(#[source] Box<dyn StdError + Send + Sync + 'static>),
+    External(#[source] ErasedError),
+}
+
+impl FunctionError {
+    pub fn by_serialization(source: impl Into<ErasedError>) -> FunctionError {
+        FunctionError::Serialization(source.into())
+    }
+
+    pub fn by_external(source: impl Into<ErasedError>) -> FunctionError {
+        FunctionError::External(source.into())
+    }
 }
